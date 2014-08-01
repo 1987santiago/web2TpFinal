f0280b7202d57bde027aca8cf382966ba365b834
Santiago <smarchioni87@gmail.com>
2014-08-01 03:16:15 -0300

    Se remueven archivos innecesarios

--- site/components/_formPrePagoReserva.php
+++
@@ -1,7 +0,0 @@
-    <h2 class="reserva">Abona tu pasaje </h2>
-    
-    <form class="contact_form" action ="<?php echo "$statics_path/sections/confirmaPago.php"; ?>" method="post" id="formulario">
-        <label>Ingresa tu código de reserva: </label>
-        <input type="text" name="codReserva" id="codReserva" />
-        <button class="submit" type="submit" >Consultar</button>
-    </form>
